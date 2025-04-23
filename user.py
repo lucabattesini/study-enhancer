4e3b801742a9612f6dc962c8629b83989e635e54
Luca Battesini <lucabattesini1@gmail.com>
2025-04-23 10:36:12 -0400

    Adding a better feedback

--- user.py
+++ user.py
@@ -1,13 +1,21 @@
 from db.questions import get_questions
+from utils import clean_terminal
 
 # Show all the questions, ask for an answer and give a feedback if you're right or wrong
 def print_questions() :
     questions = get_questions()
+    correct = int(0)
+    wrong = int(0)
     for q in questions:
+        clean_terminal()
         print(q['statement'])
+        print("=" * 10)
         answer = input("True or False? > ")
         if answer == q['correct_answer']:
-            print("You're right!")
+            correct = correct + 1
         else:
-            print("You're wrong!")
-        print("===" * 10)
+            wrong = wrong + 1
+    
+    clean_terminal()
+    print("=" * 10)
+    input(f"You did {correct} correct and {wrong} wrong")
