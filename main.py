c3cb533fb16dcda8f027f1ec4be82a21cc779b8b
Luca Battesini <lucabattesini1@gmail.com>
2025-05-07 10:18:20 -0400

    Creating an route to get a question slected by id

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 from fastapi import FastAPI
 from menus import menu_controler
-from db.questions import get_questions
+from db.questions import get_questions, get_question_selected_by_id
 
 app = FastAPI()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 @app.get("/questions")
 async def questions_list():
     return get_questions() 
 
 @app.get("/questions/{question_id}")
-async def read_question(question_id):
-    return {"question_id": question_id}
+async def get_question_by_id(question_id):
+    return get_question_selected_by_id(question_id)
 
 # to do
