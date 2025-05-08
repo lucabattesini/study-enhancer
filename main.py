32570a822c1509e796d5379549113062d823f840
Luca Battesini <lucabattesini1@gmail.com>
2025-05-08 11:42:35 -0400

    Creating a rout to delete the question  by id

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
-from menus import menu_controler
-from db.questions import get_questions, get_question_selected_by_id
+from db.questions import get_questions, get_question_selected_by_id, exclude_question
 
 app = FastAPI()
 
@@ -8,6 +7,11 @@ app = FastAPI()
 async def questions_list():
     return get_questions() 
 
-@app.get("/questions/{question_id}")
+@app.get("/questions/get/{question_id}")
 async def get_question_by_id(question_id):
     return get_question_selected_by_id(question_id)
+
+@app.get("/questions/delete/{question_id}")
+async def delete_question_by_id(question_id):
+    exclude_question(question_id)
+    return {"question deleted"}
