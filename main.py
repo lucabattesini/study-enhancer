0127fe51a4b0f3fda95dd9dfdea37ca434982aa7
Luca Battesini <lucabattesini1@gmail.com>
2025-05-12 09:53:41 -0400

    Just changing the name of some routes

--- main.py
+++ main.py
@@ -5,23 +5,41 @@ app = FastAPI()
 
 @app.get("/questions")
 async def questions_list():
+    """Return all questions"""
     return get_questions() 
 
 @app.get("/questions/get/{question_id}")
 async def get_question_by_id(question_id):
+    """
+    Return 1 question
+    /questions/{question_id}
+    """
     return get_question_selected_by_id(question_id)
 
-@app.get("/questions/delete/{question_id}")
+@app.delete("/questions/delete/{question_id}")
 async def delete_question_by_id(question_id):
+    """
+    Delete 1 question
+    @app.delete("/questions/{question_id}")
+    """
     exclude_question(question_id)
     return {"question deleted"}
 
-@app.get("/questions/edit/{object_to_change}/{change}/{question_id}")
+@app.put("/questions/edit/{object_to_change}/{change}/{question_id}")
 async def edit_question_by_id(object_to_change, change, question_id):
+    """PUT
+    @app.put("/questions/{question_id}")
+    usar body ao invés de params
+    receber o objeto inteiro com as alterações
+    fazer validações
+    """
     edit_question(question_id, object_to_change, change)
     return {"question edited"}
 
-@app.get("questions/create/{question}/{answer}/{answer_to_print}/{question_type}/{question_subject}/{question_topic}")
+@app.post("questions/create/{question}/{answer}/{answer_to_print}/{question_type}/{question_subject}/{question_topic}")
 async def create_question(question, answer, answer_to_print, question_type, question_subject, question_topic):
+    """POST
+    usar body ao invés de params
+    """
     new_question(question, answer, answer_to_print, question_type, question_subject, question_topic)
     return {"question created"}
