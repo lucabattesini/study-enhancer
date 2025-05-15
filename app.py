21347ababf919650b343a89bd68e18048efaa632
Luca Battesini <lucabattesini1@gmail.com>
2025-05-15 10:14:03 -0400

    Deleting useless notes in app.py

--- app.py
+++ app.py
@@ -1,40 +1,29 @@
 from fastapi import FastAPI
-from schemas import Question
-from db.questions import get_questions, get_question_selected_by_id, exclude_question, edit_question, new_question
+from schema.questions_schema import Question
+from repository.questions_repo import get_questions, get_question_selected_by_id, exclude_question, edit_question, new_question
 
 app = FastAPI()
 
 @app.get("/questions")
 async def questions_list():
-    """Return all questions"""
-    return get_questions() 
+    result = get_questions() 
+    print(result)
+    for r in result:
+        r.check_type()
+    return result
 
 @app.get("/questions/{question_id}")
 async def get_question_by_id(question_id):
-    """
-    Return 1 question
-    /questions/{question_id}
-    """
     return get_question_selected_by_id(question_id)
 
 @app.delete("/questions/{question_id}")
 async def delete_question_by_id(question_id):
-    """
-    Delete 1 question
-    @app.delete("/questions/{question_id}")
-    """
     exclude_question(question_id)
     return {"question deleted"}
 
-@app.put("/questions/{object_to_change}/{change}/{question_id}")
-async def edit_question_by_id(object_to_change, change, question_id):
-    """PUT
-    @app.put("/questions/{question_id}")
-    usar body ao invés de params
-    receber o objeto inteiro com as alterações
-    fazer validações
-    """
-    edit_question(question_id, object_to_change, change)
+@app.put("/questions/{question_id}")
+async def edit_question_by_id(question: Question):
+    edit_question(question)
     return {"question edited"}
 
 @app.post("/questions")
