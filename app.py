afbf6aa2ef063f23a7ddee642759eb2a167f74e1
Luca Battesini <lucabattesini1@gmail.com>
2025-05-19 21:50:02 -0400

    Making questions a sub route

--- app.py
+++ app.py
@@ -1,33 +1,11 @@
 from fastapi import FastAPI
-from schema.questions_schema import Question
-from repository.questions_repo import get_questions, get_question_selected_by_id, exclude_question, edit_question, new_question
+
+from routers import questions
 
 app = FastAPI()
 
-@app.get("/questions")
-async def questions_list():
-    result = get_questions() 
-    print(result)
-    for r in result:
-        r.check_type()
-    return result
+app.include_router(questions.router)
 
-@app.get("/questions/{question_id}")
-async def get_question_by_id(question_id):
-    return get_question_selected_by_id(question_id)
-
-@app.delete("/questions/{question_id}")
-async def delete_question_by_id(question_id):
-    exclude_question(question_id)
-    return {"question deleted"}
-
-@app.put("/questions/{question_id}")
-async def edit_question_by_id(question: Question):
-    edit_question(question)
-    return {"question edited"}
-
-@app.post("/questions")
-async def create_question(question: Question):
-    new_question(question.type, question.subject, question.topic, question.statement, 
-                question.answer_to_print, question.correct_answer)
-    return {"question created"}
+@app.get("/")
+async def root():
+    return{"message": "Hello World!"}
---
+++ routers/questions.py
@@ -0,0 +1,38 @@
+from fastapi import APIRouter
+from schema.questions_schema import Question
+from repository.questions_repo import get_questions, get_question_selected_by_id, exclude_question, edit_question, new_question
+
+
+router = APIRouter(
+    prefix="/questions",
+    tags=["questions"],
+    responses={404: {"description": "Not found"}}
+)
+
+@router.get("/")
+async def questions_list():
+    result = get_questions() 
+    print(result)
+    for r in result:
+        r.check_type()
+    return result
+
+@router.get("/{question_id}")
+async def get_question_by_id(question_id):
+    return get_question_selected_by_id(question_id)
+
+@router.delete("/{question_id}")
+async def delete_question_by_id(question_id):
+    exclude_question(question_id)
+    return {"question deleted"}
+
+@router.put("/{question_id}")
+async def edit_question_by_id(question: Question):
+    edit_question(question)
+    return {"question edited"}
+
+@router.post("/")
+async def create_question(question: Question):
+    new_question(question.type, question.subject, question.topic, question.statement, 
+                question.answer_to_print, question.correct_answer)
+    return {"question created"}
