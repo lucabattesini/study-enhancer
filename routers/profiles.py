9fa796cff51e8e3c65360a73b3485694bb7da99f
Luca Battesini <lucabattesini1@gmail.com>
2025-05-23 11:22:23 -0400

    Creating all profiles route

--- routers/profiles.py
+++ routers/profiles.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
+from repository.profiles_repo import get_profiles
 
 router = APIRouter(
-    prefix="/profile",
-    tags=["profile"],
+    prefix="/profiles",
+    tags=["profiles"],
     responses={404: {"description": "Not found"}}
 )
 
 @router.get("/")
 async def get_all_profiles():
-    return
+    result = get_profiles()
+    return result
