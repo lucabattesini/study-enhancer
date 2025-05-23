0c076450f88ad2cef99c52f87389116122318844
Luca Battesini <lucabattesini1@gmail.com>
2025-05-23 15:28:57 -0400

    Adding id to the table profiles

--- db/migrations.py
+++ db/migrations.py
@@ -3,4 +3,4 @@ from connection import cursor
 # Create a table
 cursor.execute("CREATE TABLE questions(id, type, subject, topic, statement, answers_to_print , correct_answer)")
 
-cursor.execute("CREATE TABLE profiles(name, permission, questions_answered, correct_questions)")
+cursor.execute("CREATE TABLE profiles(id, name, permission, questions_answered, correct_questions)")
