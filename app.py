9956d75f6a4b492e5dce541cab79a12cf6b78944
Luca Battesini <lucabattesini1@gmail.com>
2025-05-23 11:14:09 -0400

    Adding profiles route to the main file

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 
-from routers import questions
+from routers import questions, profiles
 
 app = FastAPI()
 
 app.include_router(questions.router)
+app.include_router(profiles.router)
 
 @app.get("/")
 async def root():
